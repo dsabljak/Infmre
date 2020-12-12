0505c7e4c8c714d0ae1400a09e9203d9c3d3d557
dsabljak <dsabljak98@gmail.com>
2020-12-12 10:59:16 +0100

    Moved file reader to main app

--- prim_dijkstra.py
+++ prim_dijkstra.py
@@ -1,11 +1,53 @@
-from file_reader import FileReader
+class FileReader:
+
+    def __init__(self, path):
+        self.file = open(path, 'r')
+        self.nodes = set()
+        self.edges = []
+        self.edge_cost = dict()
+
+        self.parse()
+
+    """
+        Data is written in .txt file like:
+        begin_node, end_node, cost, direction;
+        This method parses all rows of file and saves data
+    """
+    def parse(self):
+        data = self.file.readlines()
+
+        for row in data:
+            begin_node = row.split(',')[0]
+            end_node = row.split(', ')[1]
+            edge = begin_node + end_node
+            cost = int(row.split(', ')[2])
+            direct = row.split(', ')[3].split(';')[0]
+            self.nodes.add(begin_node)
+            self.nodes.add(end_node)
+            self.edges.append(edge)
+
+            # If cost is 0, ignore it
+            # Maybe not the best way?
+            if cost != 0:
+                self.edge_cost[edge] = cost
+
+            # If direction is not directed, then create simetric edge
+            # For example AB -> BA
+            if direct == 'n':
+                self.edge_cost[edge[::-1]] = cost
+
+        print(self.nodes)
+        print(self.edges)
+        print(self.edge_cost)
 
 # Function for getting all possible combinations for edges by nodes
 # For example [a, b] returns set {bb, aa, ab, ba} (using set to avoid redundancy)
 def get_all(nodes):
     return set([x + y for x in nodes for y in nodes] + [y + x for x in nodes for y in nodes])
 
-data = FileReader('test_data1')
+path = input("Insert path to file with data:")
+data = FileReader(path)
+
 nodes = data.nodes
 edges = data.edges
 edge_cost = data.edge_cost
